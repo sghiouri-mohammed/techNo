4923cc1e4c530cd26563b2b7fae395e0ab0e190b
sghiouri-mohammed <sghiouri.mohammed@ensam-casa.ma>
2023-03-09 00:46:29 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,22 @@ window.addEventListener("scroll", function() {
         navbar.classList.remove("navbar-white");
     }
   });
+  
+  function reveal() {
+    const reveals = document.querySelectorAll('.scroll-reveal');
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const revealTop = reveals[i].getBoundingClientRect().top;
+      const revealPoint = 150;
+      if (revealTop < windowHeight - revealPoint) {
+        reveals[i].classList.add('active');
+      } else {
+        reveals[i].classList.remove('active');
+      }
+    }
+  }
+  window.addEventListener('scroll', reveal);
+
+  
+  
+  
